ff1e2fdc0d708affafcf4dc0bd1cd6719a0fa74d
Arya Suthar <arya.suthar.deskent@gmail.com>
2025-05-15 18:16:35 +0530

    portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Arya Suthar</span> I'm a passionate Odoo developer with one year of professional experience in developing, customizing, and deploying Odoo modules. I enjoy solving business problems with clean, maintainable code."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
